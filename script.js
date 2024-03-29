72f3873f276b6eb2a0304c523fe7f7225bc1b7b7
syedamir5560 <syedamir5560@gmail.com>
2024-03-29 23:25:53 +0530

    real

--- script.js
+++ script.js
@@ -1,44 +1,62 @@
-gsap.set(".slidesm",{
-    scale:5
-})
+function homePageAnimation(){
+    gsap.set(".slidesm",{
+        scale:5
+    })
+    
+    let t1 =gsap.timeline({
+    
+        scrollTrigger: {
+            trigger: '.home',
+            start: 'top top',
+            end: 'bottom bottom',
+            marker: true,
+            scrub: 2,
+     
+    
+        }
+    
+    })
+    
+    t1.to('.vediodiv', {
+    
+        '--clip':"0%",
+        ease:Power2,
+       
+    },'a')
+    
+    t1.to('.slidesm',{
+        scale:1,
+        ease:Power2
+    
+    },'a')
+    
+    t1.to('.lft',{
+        xPercent:-10,
+        stagger:0.3,
+        ease:Power4
+    
+    },'b')
+    
+    t1.to('.rgt',{
+        xPercent:10,
+        stagger:0.3,
+        ease:Power4
+    
+    },'b')
+}
 
-let t1 =gsap.timeline({
 
-    scrollTrigger: {
-        trigger: '.home',
-        start: 'top top',
-        end: 'bottom top',
-        marker: true,
-        scrub: 2,
-        pin:true
 
-    }
-
-})
-
-t1.to('.vediodiv', {
-
-    '--clip':"0%",
-    ease:Power2,
-   
-},'a')
-
-t1.to('.slidesm',{
-    scale:1,
-    ease:Power2
-
-},'a')
-
-t1.to('.lft',{
-    xPercent:-10,
-    stagger:0.3,
+ gsap.to(".slide",{
+    scrollTrigger:{
+        trigger:'.real',
+        start:"top top",
+        end:'bottom bottom',
+        scrub:2 
+    },
+    xPercent:-200,
     ease:Power4
+ })
 
-},'b')
 
-t1.to('.rgt',{
-    xPercent:10,
-    stagger:0.3,
-    ease:Power4
-
-},'b')
+ homePageAnimation()
